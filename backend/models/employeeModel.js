77f1c7ac3022e19de9ed5dd223e1b366b98168ae
Weizhou Wen <58341131+WeizhouWen@users.noreply.github.com>
2024-04-02 22:35:46 -0700

    employeeModel in backend

---
+++ backend/models/employeeModel.js
@@ -0,0 +1,118 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+// Define the schema
+const employeeSchema = new Schema({
+  role: {
+    type: String,
+    enum: ["HR", "Employee"],
+    required: true,
+  },
+  userName: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  },
+  applicationStatus: {
+    type: String,
+    enum: ["Never submitted", "Pending", "Approved", "Rejected"],
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  middleName: String,
+  preferredName: String,
+  profilePictureLink: {
+    type: String,
+    default: "https://upload.wikimedia.org/wikipedia/commons/a/af/Default_avatar_profile.jpg",
+  },
+  currentAddress: {
+    building: String,
+    streetNumber: Number,
+    streetName: String,
+    city: String,
+    state: String,
+    zip: String,
+    required: true,
+  },
+  cellPhoneNumber: {
+    type: String,
+    required: true,
+  },
+  workPhoneNumber: String,
+  SSN: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female", "i do not wish to answer"],
+    required: true,
+  },
+  workAuth: {
+    type: String,
+    required: true,
+  },
+  workAuthDate: {
+    type: [Date],
+    default: [],
+  },
+  reference: [
+    {
+      firstName: String,
+      lastName: String,
+      middleName: String,
+      phone: String,
+      email: String,
+      relationship: String,
+    },
+  ],
+  emergencyContacts: [
+    {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+      middleName: String,
+      phone: String,
+      email: String,
+      relationship: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  documents: [String],
+  nextSteps: String,
+  feedback: String,
+});
+
+// Create a model
+const Employee = mongoose.model("Employee", employeeSchema);
+
+module.exports = Employee;
