01f79de56e4aa960a2bcfb657b78380505c92898
Weizhou Wen <58341131+WeizhouWen@users.noreply.github.com>
2024-04-04 20:58:36 -0700

    debug of hr Controller
    and some change at API

--- backend/middleware/authorization.js
+++ backend/middleware/authorization.js
@@ -1,6 +1,6 @@
 
 module.exports = async (req, res, next) => {
-  if (req.employee.role!== "HR") {
+  if (req.employee.role!== "hr") {
     console.log("Current API is only allowed for HR");
     res.status(401).json({ msg: "Unauthorize use of API" });
   }
