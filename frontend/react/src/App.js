d67c03b9606d310833ef13e409d9393c87991c2a
Weizhou Wen <58341131+WeizhouWen@users.noreply.github.com>
2024-04-06 15:12:14 -0700

    update the signup route in App.js

--- frontend/react/src/App.js
+++ frontend/react/src/App.js
@@ -45,7 +45,7 @@ export default function App() {
               >
                 <Route path="/profile" element={<PersonalProfile />} />
               </Route>
-              <Route path="/signup" element={<Signup />} />
+              <Route path="/signup/:signupToken" element={<Signup />} />
               <Route path="*" element={<Error />} />
             </Routes>
           </ErrorBoundary>
