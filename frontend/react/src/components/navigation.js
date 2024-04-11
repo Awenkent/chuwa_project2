99560122d02a20074acec29f07b8474030de290f
jianweibi <77766857+jianweibi@users.noreply.github.com>
2024-04-11 12:59:41 -0700

    Add edit and sign out logics to application status

--- frontend/react/src/components/navigation.js
+++ frontend/react/src/components/navigation.js
@@ -1,25 +1,22 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
-import {useRef} from 'react';
+import { useRef } from "react";
 
 import { useSelector, useDispatch } from "react-redux";
 import SearchIcon from "@mui/icons-material/Search";
 import ManageAccountsOutlinedIcon from "@mui/icons-material/ManageAccountsOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useNavigate } from "react-router-dom";
 
-import {
-    setEmployee,
-    selectEmployee,
-  } from "../redux/employeeSlice";
+import { setEmployee, selectEmployee } from "../redux/employeeSlice";
 
 export default function Header(props) {
   const searchRef = useRef();
@@ -36,38 +33,32 @@ export default function Header(props) {
   const handleClose = () => {
     setAnchorEl(null);
   };
-  const handleSearch = ()=>{
-    
-    if(searchRef.current.value)
-    {
-     dispatch(setProducts( products.filter((product)=>{
-        return product.productName.indexOf(searchRef.current.value) !== -1
-      })
-      ))
-    } 
-    else
-    {
+  const handleSearch = () => {
+    if (searchRef.current.value) {
+      dispatch(
+        setProducts(
+          products.filter((product) => {
+            return product.productName.indexOf(searchRef.current.value) !== -1;
+          })
+        )
+      );
+    } else {
       dispatch(fetchProducts());
     }
-    
-  
-   
-  }
-  const handleDisplayUser = ()=>{
-    console.log("handleDisplayUser")
-    dispatch(setDisplayUser("block"))
-  }
-  const handleDisplayCart = ()=>{
-    console.log("handleDisplayCart")
-    dispatch(setDisplayCart("block"))
-  }
-  const handleSignOut = ()=>
-{
-    alert("SignOut Succeeful!");
+  };
+  const handleDisplayUser = () => {
+    console.log("handleDisplayUser");
+    dispatch(setDisplayUser("block"));
+  };
+  const handleDisplayCart = () => {
+    console.log("handleDisplayCart");
+    dispatch(setDisplayCart("block"));
+  };
+  const handleSignOut = () => {
+    alert("Sign Out Successfully!");
     localStorage.removeItem("token");
     window.location.replace("/");
-}
-
+  };
 
   if (matches) {
     return (
@@ -83,98 +74,129 @@ export default function Header(props) {
         }}
       >
         <div style={{ flex: 1 }}>
-        <h3 className ="clickable" onClick = {()=>{navigate("/profile")}}>
+          <h3
+            style={{ cursor: "pointer" }}
+            className="clickable"
+            onClick={() => {
+              navigate("/profile");
+            }}
+          >
             <b>Chuwa</b> <small>Management</small>
           </h3>
         </div>
 
-
-        
-        
         <div
           style={{
             flex: 1,
             display: "flex",
             justifyContent: "center",
             alignItems: "flex-end",
-            gap:"10px",
+            gap: "10px",
             flexWrap: "nowrap",
           }}
         >
-
           {employee.role === "hr" ? (
             <>
               <span
-              id="basic-button"
-              aria-controls={open ? 'basic-menu' : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
-              onClick={handleClick}
+                id="basic-button"
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+              >
+                <Button style={{ color: "white", textTransform: "none" }}>
+                  {" "}
+                  HR Dashboard{" "}
+                </Button>
+              </span>
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "basic-button",
+                }}
+              >
+                <MenuItem
+                  onClick={() => {
+                    handleClose();
+                    navigate("/hrEmployeeProfiles");
+                  }}
+                >
+                  Employee Profiles
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    handleClose();
+                    navigate("/hr/visastatus");
+                  }}
+                >
+                  Employee Visa Status Management
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    handleClose();
+                    navigate("/hiringManagement");
+                  }}
+                >
+                  View Applications
+                </MenuItem>
+              </Menu>
+            </>
+          ) : (
+            ""
+          )}
+
+          <span
+            onClick={() => {
+              navigate("/profile");
+            }}
+          >
+            <Button style={{ color: "white", textTransform: "none" }}>
+              Profile
+            </Button>
+          </span>
+
+          <span
+            onClick={() => {
+              navigate("/visastatus");
+            }}
+          >
+            <Button style={{ color: "white", textTransform: "none" }}>
+              Visa Status
+            </Button>
+          </span>
+
+          {employee.employeeName !== null ? (
+            <span
+              className="clickable"
+              style={{ margin: "0 10px", whiteSpace: "nowrap" }}
+              onClick={handleSignOut}
             >
-             <Button style={{color:"white" ,textTransform: 'none'}}>  HR Dashboard </Button>
+              <Button style={{ color: "white", textTransform: "none" }}>
+                {" "}
+                Sign Out
+              </Button>
             </span>
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                'aria-labelledby': 'basic-button',
+          ) : (
+            <span
+              className="clickable"
+              style={{ margin: "0 10px", whiteSpace: "nowrap" }}
+              onClick={() => {
+                navigate("/signin");
               }}
             >
-              <MenuItem onClick={()=>{
-                handleClose();
-                navigate("/hrEmployeeProfiles")}
-                }>Employee Profiles</MenuItem>
-              <MenuItem onClick={()=>{
-                handleClose();
-                navigate("/hr/visastatus")}
-                }
-              >Employee Visa Status Management</MenuItem>
-               <MenuItem onClick={()=>{
-                handleClose();
-                navigate("/hiringManagement")}
-                }>View Applications</MenuItem>
-             
-            </Menu>
-            </>
-          ):""}
-
-        
-           <span onClick={()=>{navigate("/profile")}}><Button style={{color:"white" ,textTransform: 'none'}}>Profile</Button></span> 
-         
-          <span onClick={()=>{navigate("/visastatus")}}><Button style={{color:"white" ,textTransform: 'none'}}>Visa Status</Button></span> 
- 
-  
-            
-          
-          {employee.employeeName !== null?
-          (<span className ="clickable" style={{ margin: "0 10px" ,whiteSpace:"nowrap"}} onClick={handleSignOut}>
-            <Button style={{color:"white",textTransform: 'none'}}> Sign Out</Button>
-          </span>) :
-            (
-            <span className ="clickable" style={{ margin: "0 10px",whiteSpace:"nowrap" }} onClick={()=>{navigate("/signin")}}>
-             <Button style={{color:"white",textTransform: 'none'}}>  Sign in</Button>
+              <Button style={{ color: "white", textTransform: "none" }}>
+                {" "}
+                Sign in
+              </Button>
             </span>
-            )
-          }
-          
-
-        
-   
-
-         
-          
-        
-         
+          )}
         </div>
       </header>
     );
   } else {
-    return (
-      <header>
-      
-      </header>
-    );
+    return <header></header>;
   }
 }
--- frontend/react/src/features/home.js
+++ frontend/react/src/features/home.js
@@ -1,51 +1,36 @@
 import { useSelector, useDispatch } from "react-redux";
 import { Navigate, Link } from "react-router-dom";
 
-import { Outlet } from 'react-router-dom'
-import Navigation from "../components/navigation"
-import Footer from "../components/footer"
-import {
-  setEmployee,
-  selectEmployee,
-} from "../redux/employeeSlice";
+import { Outlet } from "react-router-dom";
+import Navigation from "../components/navigation";
+import Footer from "../components/footer";
+import { setEmployee, selectEmployee } from "../redux/employeeSlice";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
-export default function Home()
-{
+export default function Home() {
   const navigate = useNavigate();
-    const employee = useSelector(selectEmployee);
-    console.log(employee)
-    console.log(employee.
-      applicationStatus)
-    useEffect(()=>{
-
-      if(employee.applicationStatus !== "Approved")
-      {
-        if(employee.applicationStatus === "Never Submitted")
-        {
-        
-          navigate("/application");  
-        }
-        else
-        {
-      
-          navigate("/applicationstatus");  
-        }
+  const employee = useSelector(selectEmployee);
+  console.log(employee);
+  console.log(employee.applicationStatus);
+  useEffect(() => {
+    if (employee.applicationStatus !== "Approved") {
+      // navigate("/application");
+      if (employee.applicationStatus === "Never Submitted") {
+        navigate("/application");
+      } else {
+        navigate("/applicationstatus");
       }
-    })
+    }
+  });
 
-   
-    return (
-      <>
-      <Navigation/>
-      <div style={{minHeight: "800px"}}>
-      <Outlet />
+  return (
+    <>
+      <Navigation />
+      <div style={{ minHeight: "800px" }}>
+        <Outlet />
       </div>
-      <Footer style={{position:"fixed", bottom:"25px"}}/>
-      </>
-    )
-  
+      <Footer style={{ position: "fixed", bottom: "25px" }} />
+    </>
+  );
 }
-
-
